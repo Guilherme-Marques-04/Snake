08262b80d700cad4b7b2aa56530552c58502282e
roduit <julien.roduit@hes-so.ch>
2025-01-08 10:25:31 +0100

    test julien

--- src/Snake.scala
+++ src/Snake.scala
@@ -1,3 +1,11 @@
 object Snake extends App {
 
+  val apple: Int = 1
+  val snakeHead: Int = 2
+  val snake: Int = 3
+
+  def createArray(x:Int, y:Int): Array[Array[Int]] ={
+    val array: Array[Array[Int]] = Array.ofDim(x,y)
+    array
+  }
 }
