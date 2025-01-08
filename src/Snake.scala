b94eedfa0ae6b72cb150a7ffc61c95fd21feb7df
Guilherme Marques <marqueslote.g@gmail.com>
2025-01-08 10:25:18 +0100

    test guilherme

--- src/Snake.scala
+++ src/Snake.scala
@@ -1,3 +1,13 @@
 object Snake extends App {
+  def generateSnake(board : Array[Array[Int]]) : Array[Array[Int]] = {
 
+    var snakeSize : Int = 2
+    var out : Array[Array[Int]] = board.clone
+
+    // put the snake in the middle left of the board
+    out(1)(out(0).length) = 3
+    out(2)(out(0).length) = 2
+
+    return out
+  }
 }
