c8fffb46283cf611b7ee99525dccdc77df6b008c
Guilherme Marques <marqueslote.g@gmail.com>
2025-01-09 10:15:45 +0100

    mise en forme et amélioration de la fonction generateApple

--- src/Snake.scala
+++ src/Snake.scala
@@ -7,13 +7,13 @@ import java.awt.event.{KeyAdapter, KeyEvent}
 object Snake extends App {
   // variables
   var snakeDirection: Int = 0x27
-  var snakeHeadLocationX : Int = 0
-  var snakeHeadLocationY : Int = 0
+  var snakeHeadLocationX: Int = 0
+  var snakeHeadLocationY: Int = 0
 
-  def generateSnake(board : Array[Array[Int]]) : Array[Array[Int]] = {
+  def generateSnake(board: Array[Array[Int]]): Array[Array[Int]] = {
 
-    var snakeSize : Int = 2
-    var out : Array[Array[Int]] = board.clone
+    var snakeSize: Int = 2
+    var out: Array[Array[Int]] = board.clone
 
     // put the snake in the middle left of the board
     // body of the snake
@@ -25,7 +25,7 @@ object Snake extends App {
     return out
   }
 
-  def moveSnake(land : FunGraphics, board : Array[Array[Int]]) : Unit = {
+  def moveSnake(land: FunGraphics, board: Array[Array[Int]]): Unit = {
     val timer = new java.util.Timer()
 
     val task = new java.util.TimerTask {
@@ -45,10 +45,10 @@ object Snake extends App {
           => board(snakeHeadLocationY)(snakeHeadLocationX + 1)
         }
 
-        land.setKeyManager(new KeyAdapter(){
+        land.setKeyManager(new KeyAdapter() {
           override def keyPressed(e: KeyEvent): Unit = {
             if (e.getKeyCode == KeyEvent.VK_LEFT && snakeDirection != KeyEvent.VK_RIGHT) snakeDirection = 0x25
-            if (e.getKeyCode == KeyEvent.VK_UP && snakeDirection != KeyEvent.VK_DOWN) snakeDirection  = 0x26
+            if (e.getKeyCode == KeyEvent.VK_UP && snakeDirection != KeyEvent.VK_DOWN) snakeDirection = 0x26
             if (e.getKeyCode == KeyEvent.VK_RIGHT && snakeDirection != KeyEvent.VK_LEFT) snakeDirection = 0x27
             if (e.getKeyCode == KeyEvent.VK_DOWN && snakeDirection != KeyEvent.VK_UP) snakeDirection = 0x28
           }
@@ -69,10 +69,9 @@ object Snake extends App {
   }
 
   // generate random position of apple
-  def generateApple(): Array[Array[Int]] = {
-    var board: Array[Array[Int]] = generateSnake(createArray())
+  def generateApple(board: Array[Array[Int]]): Array[Array[Int]] = {
     var x: Int = 1
-    while(x == 1) {
+    while (x == 1) {
       var randomX: Int = Random.between(0, x)
       var randomY: Int = Random.between(0, y)
       if (board(randomX)(randomY) == 0) {
@@ -86,27 +85,27 @@ object Snake extends App {
   // display graphic
   def displayGame(): Unit = {
     val land: FunGraphics = new FunGraphics(650, 650, "Snake")
-    var table : Array[Array[Int]] = createArray()
+    var table: Array[Array[Int]] = createArray()
     table = generateSnake(table)
 
     var posX: Int = -25
     var posY: Int = -25
     var a: Array[Array[Int]] = generateApple()
 
-    for(i <- a.indices){
+    for (i <- a.indices) {
       println("")
       posY += 25
       posX = -25
-      for(j <- a(i).indices){
+      for (j <- a(i).indices) {
         print(a(i)(j))
         posX += 25
-        if(a(i)(j) == 0){
+        if (a(i)(j) == 0) {
           land.setColor(Color.blue)
-          land.drawFillRect(posX, posY, posX+25, posY+25)
-        } else if(a(i)(j) >= 1) {
+          land.drawFillRect(posX, posY, posX + 25, posY + 25)
+        } else if (a(i)(j) >= 1) {
           land.setColor(Color.green)
           land.drawFillRect(posX, posY, posX + 25, posY + 25)
-        } else if(a(i)(j) == -1){
+        } else if (a(i)(j) == -1) {
           land.setColor(Color.red)
           land.drawFillRect(posX, posY, posX + 25, posY + 25)
         }
